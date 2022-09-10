9d7d9cbaa893e216ab0a11beb42cf7ee04b2c91e
SimonIsNotAvailable <polushkinsn@gmail.com>
2022-09-10 10:40:22 +0500

    Russian sendTest

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
 
@@ -18,23 +19,19 @@ class MessageSenderImplTest {
     @Test
     void sendTest() {
         String ipAddressRussia = "172.0.32.10";
-        GeoServiceImpl geoService = Mockito.mock(GeoServiceImpl.class);
+        GeoService geoService = Mockito.mock(GeoService.class);
         Mockito.when(geoService.byIp(ipAddressRussia))
                 .thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
-
         LocalizationService localizationService = Mockito.mock(LocalizationService.class);
         Mockito.when(localizationService.locale(Country.RUSSIA))
                 .thenReturn("Добро пожаловать");
 
-
-
         String expected = "Добро пожаловать";
         MessageSenderImpl actual = new MessageSenderImpl(geoService, localizationService);
         Map<String, String> headers = new HashMap<String, String>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, ipAddressRussia);
 
         assertEquals(expected, actual.send(headers) );
-
 
     }
 }
