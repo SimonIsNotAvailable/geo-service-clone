d707e3f725671eb4cf046d8f3c26feb6951d62ad
SimonIsNotAvailable <polushkinsn@gmail.com>
2022-09-10 11:10:48 +0500

    Params for russian IP

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -1,6 +1,8 @@
 package ru.netology.sender;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
@@ -16,11 +18,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MessageSenderImplTest {
 
-    @Test
-    void sendTest() {
-        String ipAddressRussia = "172.0.32.10";
+    @ParameterizedTest
+    @ValueSource(strings = {"172.0.32.10", "172.0.23.01", "172.123.12.19", "172.16.13.98"})
+    void sendTest(String ip) {
+
         GeoService geoService = Mockito.mock(GeoService.class);
-        Mockito.when(geoService.byIp(ipAddressRussia))
+        Mockito.when(geoService.byIp(ip))
                 .thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
         LocalizationService localizationService = Mockito.mock(LocalizationService.class);
         Mockito.when(localizationService.locale(Country.RUSSIA))
@@ -29,7 +32,7 @@ class MessageSenderImplTest {
         String expected = "Добро пожаловать";
         MessageSenderImpl actual = new MessageSenderImpl(geoService, localizationService);
         Map<String, String> headers = new HashMap<String, String>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, ipAddressRussia);
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, ip);
 
         assertEquals(expected, actual.send(headers) );
 
